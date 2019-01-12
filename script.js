c850ccff5ba93c7383eb92348c1f3df3a3ef6ed9
PaulinaKomorek <46355739+PaulinaKomorek@users.noreply.github.com>
2019-01-12 21:54:54 +0100

    error check

--- script.js
+++ script.js
@@ -5,45 +5,47 @@ var operation = "";
 var selectedNumber = "first";
 
 function setOperation(Operation) {
-    operation = Operation;
-    selectedNumber = "second";
+	if (selectedNumber == "first")
+		selectedNumber = "second";
+	else
+		setEqual();
+	operation = Operation;
 }
 
 function setEqual() {
-    if (operation == "Add")
-        result = firstNumber + secondNumber;
-    else if
-    (operation == "Substraction")
-        result = firstNumber - secondNumber;
-    else if
-    (operation == "Multiplication")
-        result = firstNumber * secondNumber;
-    else if (operation == "Division")
-        result = firstNumber / secondNumber;
-    firstNumber = result;
-    secondNumber = 0;
-
-
-
-
-    document.getElementById("display").value = result;
+	if (operation == "Add")
+		result = firstNumber + secondNumber;
+	else if
+		(operation == "Substraction")
+		result = firstNumber - secondNumber;
+	else if
+		(operation == "Multiplication")
+		result = firstNumber * secondNumber;
+	else if (operation == "Division")
+		if (secondNumber == 0)
+			alert("Error!");
+		else
+			result = firstNumber / secondNumber;
+	firstNumber = result;
+	secondNumber = 0;
+	document.getElementById("display").value = result;
 }
 
 function setNumber(Number) {
-    if (selectedNumber == "first") {
-        firstNumber = firstNumber * 10 + Number;
-        document.getElementById("display").value = firstNumber;
-    }
-    else {
-        secondNumber = secondNumber * 10 + Number;
-        document.getElementById("display").value = secondNumber;
-    }
+	if (selectedNumber == "first") {
+		firstNumber = firstNumber * 10 + Number;
+		document.getElementById("display").value = firstNumber;
+	}
+	else {
+		secondNumber = secondNumber * 10 + Number;
+		document.getElementById("display").value = secondNumber;
+	}
 }
 
 function clearResult() {
-    firstNumber = 0;
-    secondNumber = 0;
-    selectedNumber = "first";
-    document.getElementById("display").value = " ";
+	firstNumber = 0;
+	secondNumber = 0;
+	selectedNumber = "first";
+	document.getElementById("display").value = " ";
 }
 
